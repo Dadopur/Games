f987959ba364a674162eccbe636eb445878a0069
Daniel Purgal <danpu323@student.liu.se>
2024-04-13 11:33:14 +0200

    add my files

---
+++ build/CMakeFiles/main.dir/hello_world.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/hello_world.cpp.o: \
+ /home/daniel/private_games/hello_world.cpp /usr/include/stdc-predef.h \
+ /usr/local/include/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
